7d6128dd1967bc11746142bb7597428fa47b5de7
HuJian <502361472@qq.com>
2021-06-08 14:56:33 +0800

    插入排序 交换位置 移动位置

---
+++ src/main/java/com/hjrpc/sort/InsertSort.java
@@ -0,0 +1,69 @@
+package com.hjrpc.sort;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] simpleArray = ArrayDataUtil.getSimpleArray(10);
+        insertSort(simpleArray);
+//        insertSortOptimized(simpleArray);
+//        insertSortOptimized2(simpleArray);
+        ArrayDataUtil.show(simpleArray);
+
+        long l = System.currentTimeMillis();
+        insertSort(ArrayDataUtil.getAdvanceArray(80000));
+        System.out.println("插入排序,共耗时:" + (System.currentTimeMillis() - l));
+
+        long l2 = System.currentTimeMillis();
+        insertSortOptimized(ArrayDataUtil.getAdvanceArray(80000));
+        System.out.println("插入排序优化的,共耗时:" + (System.currentTimeMillis() - l2));
+
+        long l3 = System.currentTimeMillis();
+        //这个优化反而慢了
+        insertSortOptimized2(ArrayDataUtil.getAdvanceArray(80000));
+        System.out.println("插入排序优化2的,共耗时:" + (System.currentTimeMillis() - l3));
+    }
+
+    /**
+     * 插入排序,思想,默认分为两个数组,左边为有序数组,默认只有一个元素,右边为无序,将右边的元素逐一按顺序插入左边的有序数组
+     *
+     * @param arr
+     */
+    private static void insertSort(int[] arr) {
+        //从第二个数开始插入左边的数组
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            //往左还有元素,并且当前这个元素小于左边的元素,我们就交换 或者记录位置
+            while (j - 1 >= 0 && arr[j] < arr[j - 1]) {
+                ArrayDataUtil.swap(arr, j, j - 1);
+                j--;
+            }
+        }
+    }
+
+    private static void insertSortOptimized(int[] arr) {
+        //从第二个数开始插入左边的数组
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            int temp = arr[j];
+            while (j - 1 >= 0 && temp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    private static void insertSortOptimized2(int[] arr) {
+        //从第二个数开始插入左边的数组
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            int temp = arr[j];
+            while (j - 1 >= 0 && temp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            if (i != j) {
+                arr[j] = temp;
+            }
+        }
+    }
+}
--- src/main/java/com/hjrpc/sort/SelectSort.java
+++ src/main/java/com/hjrpc/sort/SelectSort.java
@@ -1,13 +1,31 @@
 package com.hjrpc.sort;
 
+/**
+ * 选择排序
+ */
 public class SelectSort {
     public static void main(String[] args) {
         int[] simpleArray = ArrayDataUtil.getSimpleArray(10);
         selectSort(simpleArray);
         ArrayDataUtil.show(simpleArray);
+
+        long l = System.currentTimeMillis();
+        selectSort(ArrayDataUtil.getAdvanceArray(80000));
+        System.out.println("选择排序,共耗时:" + (System.currentTimeMillis() - l));
     }
 
     private static void selectSort(int[] arr) {
-
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            int minValue = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < minValue) {
+                    minIndex = j;
+                    minValue = arr[j];
+                }
+            }
+            arr[minIndex] = arr[i];
+            arr[i] = minValue;
+        }
     }
 }
