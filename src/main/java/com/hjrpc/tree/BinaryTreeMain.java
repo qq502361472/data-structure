0a5f687223ec33e7e281a7758b33f1e93a69c60a
HuJian <502361472@qq.com>
2021-06-16 15:23:36 +0800

    二叉树的遍历 前序/中序/后序 二叉树的查找 前序/中序/后序

--- src/main/java/com/hjrpc/tree/BinaryTreeMain.java
+++ src/main/java/com/hjrpc/tree/BinaryTreeMain.java
@@ -4,6 +4,7 @@ package com.hjrpc.tree;
  * 二叉树的前序,中序,后序遍历
  */
 public class BinaryTreeMain {
+    static int times = 0;
 
     public static void main(String[] args) {
         Node node1 = new Node(1);
@@ -28,6 +29,12 @@ public class BinaryTreeMain {
         binaryTree.preOrder();   //1-2-3-4-5
         binaryTree.infixOrder(); //2-1-4-3-5
         binaryTree.postOrder();  //2-4-5-3-1
+
+        int searchNum = 8;
+        System.out.println("前序遍历查询到节点:" + binaryTree.preOrderSearch(searchNum) + ",共查了[" + times + "]次!");
+        System.out.println("中序遍历查询到节点:" + binaryTree.infixOrderSearch(searchNum) + ",共查了[" + times + "]次!");
+        System.out.println("后序遍历查询到节点:" + binaryTree.postOrderSearch(searchNum) + ",共查了[" + times + "]次!");
+
     }
 
 
@@ -73,6 +80,39 @@ public class BinaryTreeMain {
             System.out.println();
             System.out.println("--------------------");
         }
+
+        //前序遍历查找
+        public Node preOrderSearch(int no) {
+            times = 0;
+            System.out.print("前序遍历查找:");
+            if (root == null) {
+                System.out.println("二叉树是空的...");
+                return null;
+            }
+            return root.preOrderSearch(no);
+        }
+
+        //中序遍历查找
+        public Node infixOrderSearch(int no) {
+            times = 0;
+            System.out.print("中序遍历查找:");
+            if (root == null) {
+                System.out.println("二叉树是空的...");
+                return null;
+            }
+            return root.infixOrderSearch(no);
+        }
+
+        //后序遍历查找
+        public Node postOrderSearch(int no) {
+            times = 0;
+            System.out.print("后序遍历查找:");
+            if (root == null) {
+                System.out.println("二叉树是空的...");
+                return null;
+            }
+            return root.postOrderSearch(no);
+        }
     }
 
     static class Node {
@@ -93,7 +133,7 @@ public class BinaryTreeMain {
 
         //前序遍历
         public void preOrder() {
-            System.out.print(" "+id+" ");
+            System.out.print(" " + id + " ");
             if (left != null) {
                 left.preOrder();
             }
@@ -107,13 +147,13 @@ public class BinaryTreeMain {
             if (left != null) {
                 left.infixOrder();
             }
-            System.out.print(" "+id+" ");
+            System.out.print(" " + id + " ");
             if (right != null) {
                 right.infixOrder();
             }
         }
 
-        //后序遍历
+        //后序遍历查找
         public void postOrder() {
             if (left != null) {
                 left.postOrder();
@@ -121,7 +161,69 @@ public class BinaryTreeMain {
             if (right != null) {
                 right.postOrder();
             }
-            System.out.print(" "+id+" ");
+            System.out.print(" " + id + " ");
+        }
+
+        //前序遍历查找
+        public Node preOrderSearch(int no) {
+            times++;
+            if (this.id == no) {
+                return this;
+            }
+            Node res = null;
+            if (this.left != null) {
+                res = this.left.preOrderSearch(no);
+            }
+
+            if (res == null && this.right != null) {
+                res = this.right.preOrderSearch(no);
+            }
+            return res;
+        }
+
+        //中序遍历查找
+        public Node infixOrderSearch(int no) {
+            Node res = null;
+            if (this.left != null) {
+                res = this.left.infixOrderSearch(no);
+            }
+            if (res != null) {
+                return res;
+            }
+            times++;
+            if (this.id == no) {
+                return this;
+            }
+
+            if (this.right != null) {
+                res = this.right.infixOrderSearch(no);
+            }
+            return res;
+        }
+
+        //后序遍历
+        public Node postOrderSearch(int no) {
+            Node res = null;
+            if (this.left != null) {
+                res = this.left.postOrderSearch(no);
+            }
+            if (res != null) {
+                return res;
+            }
+
+            if (this.right != null) {
+                res = this.right.postOrderSearch(no);
+            }
+
+            if (res != null) {
+                return res;
+            }
+
+            times++;
+            if (this.id == no) {
+                return this;
+            }
+            return res;
         }
     }
 }
