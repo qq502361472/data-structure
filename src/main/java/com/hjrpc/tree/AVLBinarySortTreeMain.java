31771fe5b0995fa5f4268b9286233f40bbcc59a4
HuJian <502361472@qq.com>
2021-06-27 11:16:14 +0800

    平衡二叉排序树例子数组提交

---
+++ src/main/java/com/hjrpc/tree/AVLBinarySortTreeMain.java
@@ -0,0 +1,10 @@
+package com.hjrpc.tree;
+
+public class AVLBinarySortTreeMain {
+    public static void main(String[] args) {
+//        int[] arr = {4, 3, 6, 5, 7, 8};
+//        int[] arr = {10, 12, 8, 9, 7, 6};
+//        int[] arr = {10,11, 7, 6, 8, 9};
+        int[] arr = {2, 1, 6, 5, 7, 3};
+    }
+}
