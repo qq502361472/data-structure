7e95acfcd03bae2d0159ebd276fb4eda17782616
HuJian <502361472@qq.com>
2021-07-01 11:24:36 +0800

    动态规划,背包问题解决

---
+++ src/main/java/com/hjrpc/dynamic/KnapsackProblem.java
@@ -0,0 +1,14 @@
+package com.hjrpc.dynamic;
+
+public class KnapsackProblem {
+    public static void main(String[] args) {
+        //物品的重量
+        int[] weight = {1, 3, 4};
+        //物品的单价
+        int[] price = {1500, 2000, 3000};
+        //背包的容量
+        int knapsackCapacity = 4;
+        //物品的数量
+        int num = price.length;
+    }
+}
