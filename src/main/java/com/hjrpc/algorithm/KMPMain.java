4bdcb863b32940d62644679d091e06918f52b542
HuJian <502361472@qq.com>
2021-07-04 13:13:42 +0800

    kmp

--- src/main/java/com/hjrpc/algorithm/KMPMain.java
+++ src/main/java/com/hjrpc/algorithm/KMPMain.java
@@ -1,5 +1,7 @@
 package com.hjrpc.algorithm;
 
+import java.util.Arrays;
+
 /**
  * 查找字符串,KMP算法
  */
@@ -7,9 +9,11 @@ public class KMPMain {
     public static void main(String[] args) {
         //               012345678901234567890
         String source = "BBCABCDABABCDABCDABDE";
-        String target = "ABCDABD";
-        int index = violenceMatch(source, target);
-        System.out.println(index);
+        String target = "ABCDABB";
+//        int index = violenceMatch(source, target);
+//        System.out.println(index);
+        int[] ints = KMPNext(target);
+        System.out.println(Arrays.toString(ints));
     }
 
     private static int violenceMatch(String source, String target) {
@@ -32,5 +36,29 @@ public class KMPMain {
             }
         }
         return -1;
+    }
+
+
+    //ABCDABD
+    static public int[] KMPNext(String target) {
+        char[] chars = target.toCharArray();
+        int[] next = new int[chars.length];
+        //初始化第一个位置为0，因为只有一个字母的时候，公共前后缀是0
+        next[0] = 0;
+        //最长公共前后坠
+        //i代表前缀的最后一个下标，因为是从1开始，所以前缀的长度也是i
+        for (int i = 1,j = 0; i < chars.length; i++) {
+            while (j > 0 && chars[i] != chars[j]) {
+                //这里看不懂
+                j = next[j - 1];
+            }
+
+            //当前缀的最后一个字母 = 后缀的最后一个字母
+            if (chars[i] == chars[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
     }
 }
